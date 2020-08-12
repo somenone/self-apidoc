34a32f003471f9005137a118245457d5d7c76548
scq <chaoqun_sheng@163.com>
2020-08-12 19:17:45 +0800

    tree-node

---
+++ src/views/Left/components/treenew.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="tree">
+    <div class="tree-name">
+      <span>
+      {{tree.name}}
+      </span>
+    </div>
+
+    <tree-node class="tree-node" v-for="(item,index)in tree.tree" :key="index">
+    </tree-node>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"treeNode"
+  props:["tree"],
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
